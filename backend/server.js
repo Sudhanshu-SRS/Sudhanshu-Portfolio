cc5cc69c56d7c71fee01dd0ee0c6af71d4f58d15
Sudhanshu Sakhare <sudhanshusakhare808@gmail.com>
2026-03-31 02:02:15 +0530

    Fixed:Case Sensitivity

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const dns=require('dns')
 dns.setServers(['8.8.8.8','2001:4860:4860::8888'])
 const app=require('./src/app.js')
 const DB=require('./src/db/dbconnect.js')
-const Port=process.env.Port||5000;
+const PORT=process.env.Port||5000;
 const CreateAdmin=require('./src/utils/admincreator.js')
 
 DB().then(()=>{
 CreateAdmin();
-app.listen(Port,()=>{
-    console.log(`Server Running On ${Port}` );
+app.listen(PORT,()=>{
+    console.log(`Server Running On ${PORT}` );
 })
 
 });
