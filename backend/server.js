403e2e5e6dbedb57d19964b9782535cf783a7bf8
Sudhanshu Sakhare <sudhanshusakhare808@gmail.com>
2026-03-31 02:21:05 +0530

    Fixed:Path Naming issue

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dns=require('dns')
 dns.setServers(['8.8.8.8','2001:4860:4860::8888'])
 const app=require('./src/app.js')
 const DB=require('./src/db/dbconnect.js')
-const PORT=process.env.Port||5000;
+const PORT=process.env.PORT||5000;
 const CreateAdmin=require('./src/utils/admincreator.js')
 
 DB().then(()=>{
