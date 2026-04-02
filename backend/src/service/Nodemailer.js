cd0b67f3a1a36766774a12b21edaf28691b04e63
Sudhanshu Sakhare <sudhanshusakhare808@gmail.com>
2026-04-02 15:43:15 +0530

    Fixed:Creditianls Problem

--- backend/src/service/Nodemailer.js
+++ backend/src/service/Nodemailer.js
@@ -4,8 +4,8 @@ const SendEmail=async(Email,Otp)=>{
     const Transporter=nodemailer.createTransport({
         service:'gmail',
         auth:{
-            user:process.env.Email_User,
-            pass:process.env.Email_Pass
+            user:process.env.Email_USER,
+            pass:process.env.Email_PASS
         }
     })
 
